a28a06fb5a1bd655224e5f56c88f19d70959035c
david <dgallula@gmail.com>
2024-08-07 23:31:57 +0300

    first commit

---
+++ vue-project1/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Count is: {{ count }}</p>
+  <div v-if="count >5 " class="text-red-500">
+    <p>You clicked more than 5 times</p>
+  </div>
+  <button @click="increment">Incrementer</button>
+  <button @click="decrement">Décrémenter</button>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+// Définir la variable réactive 'count'
+const count = ref(0)
+
+// Définir la fonction 'increment'
+const increment = () => {
+  count.value++
+}
+
+const decrement = () => {
+  count.value--
+}
+
+</script>
+
+<style>
+/* Styles ici */
+</style>
+
+
+
